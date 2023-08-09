a4fa55c7c3721fe1c1f43ca50ff0c2d33540cfef
thieudxqph21179 <115914547+thieudxqph21179@users.noreply.github.com>
2023-08-09 16:05:52 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "react-loading-skeleton/dist/skeleton.css";
+import './App.css'
+function App() {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/store/index.ts
@@ -0,0 +1,46 @@
+
+import { Action, ThunkAction, combineReducers, configureStore } from '@reduxjs/toolkit';
+import {
+    FLUSH,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+    REHYDRATE,
+    persistReducer,
+    persistStore,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart']
+}
+const rootReducer = combineReducers({
+    
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }).concat(),
+})
+
+export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>
+export type AppThunk<ReturnType = void> = ThunkAction<
+    ReturnType,
+    RootState,
+    unknown,
+    Action<string>
+>
+export default persistStore(store);
+
